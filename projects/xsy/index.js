cb07d856255eb96e0994a370f68232c6f68a0f8c
Brandon Aaskov <445778+brandonaaskov@users.noreply.github.com>
2025-06-09 12:29:46 -0400

    XSY: adds TVL tracking for UTY and yUTY

---
+++ projects/xsy/index.js
@@ -0,0 +1,41 @@
+const { sumTokens2 } = require('../helper/unwrapLPs')
+
+const MANAGEMENT_CONTRACT = '0x9B9cF4f6255f6b451132Bdf7a3682c7299D4c77A'
+const UTY_TOKEN = '0xDBc5192A6B6FfEe7451301bb4ec312f844F02B4A'
+const USDC_TOKEN = '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E'
+const USDT_TOKEN = '0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7'
+const AVAX_TOKEN = '0x0000000000000000000000000000000000000000'
+const PHARAOH_POOL_USDC_UTY = '0xca7bd86983a2474f3c7cb45207bc16760cee07cf'
+const EULER_UTY_VAULT = '0x902714C7661697c873C76dEC426b63B2593ecc0a'
+
+async function tvl(api) {
+    // track total UTY supply
+    const utyTotalSupply = await api.call({
+        abi: 'erc20:totalSupply',
+        target: UTY_TOKEN,
+    })
+    api.add(UTY_TOKEN, utyTotalSupply)
+
+    // track other protocol assets (non-UTY tokens since UTY total supply is tracked above)
+    await sumTokens2({
+        api,
+        tokensAndOwners: [
+            // Management contract deposits awaiting conversion
+            [USDC_TOKEN, MANAGEMENT_CONTRACT],
+            [AVAX_TOKEN, MANAGEMENT_CONTRACT],
+            // DEX pool non-UTY tokens
+            [USDC_TOKEN, PHARAOH_POOL_USDC_UTY],
+            // Euler vault non-UTY tokens
+            [USDT_TOKEN, EULER_UTY_VAULT],
+            // Note: Not counting yUTY as it represents shares of underlying UTY (already counted in total supply)
+        ],
+    })
+}
+
+module.exports = {
+    methodology: 'TVL consists of total UTY token supply, USDC deposits in management contract awaiting conversion, USDC in DEX pools, and USDT in Euler vaults.',
+    start: 58017291, // block when UTY contract was deployed
+    avax: {
+        tvl,
+    }
+}
