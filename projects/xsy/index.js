69583e996aa3c36317204a1db06b4eb7827bcd9a
Brandon Aaskov <445778+brandonaaskov@users.noreply.github.com>
2025-06-12 09:54:39 -0400

    removes pharaoh from TVL tracking
    "this counts as Pharoah TVL and cant also count as yours"
    https://github.com/DefiLlama/DefiLlama-Adapters/pull/14996#pullrequestreview-2912523902

--- projects/xsy/index.js
+++ projects/xsy/index.js
@@ -4,7 +4,6 @@ const MANAGEMENT_CONTRACT = '0x9B9cF4f6255f6b451132Bdf7a3682c7299D4c77A'
 const UTY_TOKEN = '0xDBc5192A6B6FfEe7451301bb4ec312f844F02B4A'
 const USDC_TOKEN = '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E'
 const AVAX_TOKEN = '0x0000000000000000000000000000000000000000'
-const PHARAOH_POOL_USDC_UTY = '0xca7bd86983a2474f3c7cb45207bc16760cee07cf'
 
 async function tvl(api) {
     // track total UTY supply
@@ -21,9 +20,6 @@ async function tvl(api) {
             // Management contract deposits awaiting conversion
             [USDC_TOKEN, MANAGEMENT_CONTRACT],
             [AVAX_TOKEN, MANAGEMENT_CONTRACT],
-            // DEX pool non-UTY tokens
-            [USDC_TOKEN, PHARAOH_POOL_USDC_UTY],
-            // Note: Not counting yUTY as it represents shares of underlying UTY (already counted in total supply)
         ],
     })
 }
